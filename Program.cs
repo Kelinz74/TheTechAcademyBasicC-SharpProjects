33c51d9b82d0b1c54b5ba587593ae30cbef8afeb
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-10-28 19:20:40 -0700

    Writing and Running Code In C# Assignment

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Ssytems\nRemember, We're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
