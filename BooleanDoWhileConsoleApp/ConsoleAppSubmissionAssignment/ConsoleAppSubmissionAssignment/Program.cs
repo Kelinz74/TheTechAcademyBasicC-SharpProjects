c8529265bf0f921eace86474c289a49c4e87fdfe
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-11-03 11:34:49 -0700

    Changed the program to show a while loop and then a "Do" while loop.

--- BooleanDoWhileConsoleApp/ConsoleAppSubmissionAssignment/ConsoleAppSubmissionAssignment/Program.cs
+++ BooleanDoWhileConsoleApp/ConsoleAppSubmissionAssignment/ConsoleAppSubmissionAssignment/Program.cs
@@ -10,8 +10,21 @@ namespace ConsoleAppSubmissionAssignment
     {
         static void Main(string[] args)
         {
-            //a WriteLine method asking for an input of an int value 1 - 5.  The input will be saved to the variable number converted from a string to an int value.            
-            Console.WriteLine("What is your favorite number from 1 - 5?");
+            //a bool variable that starts out with the boolean value false
+            bool answer = false;
+            //a while statement that as long as answer equals false, the quest from Console.WriteLine("Do you like guessing games? (true or false)"); printing to the console until
+            //you type true to change the variable answer to the boolean value of true.
+            while (!answer)
+            {
+
+                Console.WriteLine("Do you like guessing games? (true or false)");
+                bool trueOrfalse = Convert.ToBoolean(Console.ReadLine());
+                answer = trueOrfalse;
+
+            }
+
+            //a WriteLine method asking for an input of an int value 1 - 40.  The input will be saved to the variable number converted from a string to an int value.            
+            Console.WriteLine("Guess what number I am thinking of between 1 - 40?");
             int number = Convert.ToInt32(Console.ReadLine());
             //a boolean variable witht he bool value false
             bool isGuessed = false;
@@ -20,46 +33,48 @@ namespace ConsoleAppSubmissionAssignment
             //will continue.
             do
             {
-                //switch statment for the variable number with different cases for the input of 1 - 5 and a final default which will not change the while loop requirment to true
-                //therefore looping back through the switch option until an input of 1 - 5 is chosen.  for each of the 1 -5 options a Writeline will display to the console and 
-                //then change the isGuessed to true.  The key word break will then stop the switch from proceeding through further cases of the switch statement.
+                //switch statment for the variable number with different cases for the input of 1 - 40 and a default that is the extra option if none of the cases are met.  Only one case will 
+                //
                 switch (number)
                 {
-                    case 1:
-                        Console.WriteLine("Ya, thats not bad");
+                    case 13:
+                        Console.WriteLine("Further");
+                        Console.WriteLine("Guess what number I am thinking of between 1 - 40?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 17:
+                        Console.WriteLine("Closer");
+                        Console.WriteLine("Guess what number I am thinking of between 1 - 40?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 20:
+                        Console.WriteLine("You got it!!!");
                         isGuessed = true;
                         break;
-                    case 2:
-                        Console.WriteLine("Well thats not to bad but you didn't choose 1");
-                        isGuessed = true;
+                    case 25:
+                        Console.WriteLine("Closer!");
+                        Console.WriteLine("Guess what number I am thinking of between 1 - 40?");
+                        number = Convert.ToInt32(Console.ReadLine());
                         break;
-                    case 3:
-                        Console.WriteLine("Always in the middle!!");
-                        isGuessed = true;
-                        break;
-                    case 4:
-                        Console.WriteLine("Over achiever obvoiusly!");
-                        isGuessed = true;
-                        break;
-                    case 5:
-                        Console.WriteLine("5!!!!  Jinx, you owe me a coke!");
-                        isGuessed = true;
+                    case 37:
+                        Console.WriteLine("Further");
+                        Console.WriteLine("Guess what number I am thinking of between 1 - 40?");
+                        number = Convert.ToInt32(Console.ReadLine());
                         break;
                     default:
-                        Console.WriteLine("You really should have chose a number 1 - 5.");
-                        Console.WriteLine("\nWhat is your favorite number from 1 - 5?");
+                        Console.WriteLine("Wrong, try again!");
+                        Console.WriteLine("Guess what number I am thinking of between 1 - 40?");
                         number = Convert.ToInt32(Console.ReadLine());
                         break;
 
                 }
             }
 
+
             //a while loop to itterate through the switch until the bool value is changed to True !isGuessed = false.  the ! represents Not True.  The loop will continue until
             //true
             while (!isGuessed);
             {
-
-
             }
             //pauses the program from closing so the output can be read.  To close the program, press the enter key one more time.
             Console.ReadLine();
