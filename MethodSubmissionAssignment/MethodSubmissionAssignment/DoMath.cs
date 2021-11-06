56897f7a2c3231ab90527bed8c7d23ffcc4357c8
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-11-06 16:22:21 -0700

    Created a class.  In that class, created a method that takes two integers are parameters.  One of the parameters were optional as an input being passed in as 0.  WIth requests in the console, asked the user to iinput two numbers one at a time.  The second input could be left black if the user didn't want to input a second number.  Made the call to the method in the class, passing in the one or two numbers entered.  If the second number was black, it was automaticaly supplied with a 0 as the value as stated from above.

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/DoMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    //class DoMath
+    class DoMath
+    {
+        //public int method named addition taking in two parameters, both being int values.  If a second value is not passed in, it will automaticly equal 0.  an addition calculation is done with 
+        // the two values passed in and the rusult is saved to the variable donemath and then returned as the result of the addition method.
+        public int addition(int num1, int num2 = 0)
+        {
+            int donemath = num1 + num2;
+            return donemath;
+        }
+    }
+}
---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class of DoMath
+            DoMath doMath = new DoMath();
+
+            //two variable representing inputs from the user
+            int input1 = 0;
+            int optionalinput = 0;
+
+         
+            //a WriteLine method printing to the consol a request for an int saving the input to the variable input1
+            Console.WriteLine("Please enter a number");
+            input1 = Convert.ToInt32(Console.ReadLine());
+
+            //a WriteLine method printing to the consol a request for an int with the possibility of leaving it blank.
+            Console.WriteLine("Please enter a number if you would like one more.\n  Leave blank and press enter if not.");
+            
+            //a bool variable holding a value of true or false from a TryParse method to check if the input was succesfuly converted to an int value.
+            bool isnum = int.TryParse(Console.ReadLine(), out optionalinput);
+            //if isnum = false, optionalinput = 0,  if the previouse TryParse was able to convert a string to an int value, the value was saved to the variable optionalinput
+            if (!isnum)
+            {
+                optionalinput = 0;
+            }
+
+            //if optionalinput = 0 the following WriteLine method will print to the consol the result of the method addition in the doMath class with 1 value passed in.  The result of the 
+            //calculation being returned concatinated into the Writeline.
+            if (optionalinput == 0)
+            {
+                Console.WriteLine("You entered one number. " + input1 + " + nothing = " + doMath.addition(input1));
+            }
+            //else if two values were provided, both values were passed to the method addition for an addition calculation to be performed with the result being return can concatinated into the 
+            //WriteLine method
+            else
+            {
+                Console.WriteLine("You entered two numbers. " + input1 + " + " + optionalinput + " = " + doMath.addition(input1, optionalinput));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
