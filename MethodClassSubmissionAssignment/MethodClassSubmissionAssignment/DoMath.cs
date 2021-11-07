823b380c6f21e4e1e1057977b00622c426128dd8
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-11-06 17:37:18 -0700

    Created a class.  In that class, created a void method that takes two integers as parameters with the method performing a math calculation on the first integer and displying the second integer to the screen.  In the main() method of the console app, instantiated the class.  Called the method in the class passing in tow numbers.  Called the method in the class specifying the parameters by name.

---
+++ MethodClassSubmissionAssignment/MethodClassSubmissionAssignment/DoMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    //public class DoMath
+    public class DoMath
+    {
+        //a public void method that does not return a value.  The results anything done and or displayed in from the method itself.  An addition calculation is done to num1 and saved to the 
+        //variable mathdone.  num2 is printed to the console using the WriteLive method.  The method is being called from the main() Program.cs
+        public void addition(int num1, int num2)
+        {
+            int mathdone = num1 + 55;            
+            Console.WriteLine(num2);            
+        }
+        
+    }
+}
---
+++ MethodClassSubmissionAssignment/MethodClassSubmissionAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the class DoMath
+            DoMath domath = new DoMath();
+
+            //a call to the method in class domath with int values being passed as the parameters.
+            domath.addition(25, 75);
+            Console.ReadLine();
+            //a call to the method in class domath with the parameters set by name with thier values.
+            domath.addition(num1: 37, num2: 100);
+            Console.ReadLine();
+        }
+    }
+}
