bd691558753b1b7fbe19019a359557313b00faea
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-11-07 17:25:13 -0800

    Created an abstract class called Person with two string properties.  Created a  method named SayName().  Created a class called Employee to inherit from the person class.  Implemented the SayName method inside of the employee class.  In the Main() method, instantiated the Empolyee object with firstName sample and lastName Student.  Called the SayName() method on the object.

---
+++ AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmissionAssignment
+{
+    //Public class Employee inheriting from Person class
+    public class Employee : Person
+    {
+        //this method overrides the SayName method from Person class
+        public override void SayName()
+        {            
+            Console.WriteLine(firstName + " " + lastName);
+        }
+    }
+}
+    
---
+++ AbstractClassSubmissionAssignment/AbstractClassSubmissionAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate class employee passing in the properties firstName and lastName
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };
+
+            //call to the method SayName
+            employee.SayName();
+            Console.ReadLine();
+
+        }
+    }
+}
