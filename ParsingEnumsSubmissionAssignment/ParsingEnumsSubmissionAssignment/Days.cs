f46c12940c59ab69a0940524fb99d1d917606014
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-11-09 01:16:34 -0800

    Parsing Enums Submission Assignment.  Created an enum for the days of the week.  Prompted the suer to enter the current day of the week.  Assign the value to the variable of the enum data type.  Wrapped the statement ina try/catch block, had the catch print to the console "Please enter an accual day of the week."

---
+++ ParsingEnumsSubmissionAssignment/ParsingEnumsSubmissionAssignment/Days.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsSubmissionAssignment
+{
+    class Days
+    {
+
+        
+        //public enum with values of the days of the week as string values
+        public enum daysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ ParsingEnumsSubmissionAssignment/ParsingEnumsSubmissionAssignment/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ParsingEnumsSubmissionAssignment
+{
+    class Program : Days
+    {
+        static void Main(string[] args)
+        {
+
+            //try for a try catch for error catching to print to the console a more user friendly statment.
+            try
+            {
+                //writeline statment printing to the console a request for input from the user
+                Console.WriteLine("What day of the week is it today?");
+                string dayChoice = Console.ReadLine().ToLower();
+
+                //applying the enum value to a variable
+                daysOfTheWeek days1 = daysOfTheWeek.Monday;
+                daysOfTheWeek days2 = daysOfTheWeek.Tuesday;
+                daysOfTheWeek days3 = daysOfTheWeek.Wednesday;
+                daysOfTheWeek days4 = daysOfTheWeek.Thursday;
+                daysOfTheWeek days5 = daysOfTheWeek.Friday;
+                daysOfTheWeek days6 = daysOfTheWeek.Saturday;
+                daysOfTheWeek days7 = daysOfTheWeek.Sunday;
+
+                //comparing the enum value to the input value from the user to print a writeline statement
+                if (dayChoice == "monday")
+                {
+                    Console.WriteLine("\n" + days1 + ", That is today, good guess.");
+                }
+                else if (dayChoice == "tuesday")
+                {
+                    Console.WriteLine("\n" + days2 + ", That is today, good guess.");
+                }
+                else if (dayChoice == "wednesday")
+                {
+                    Console.WriteLine("\n" + days3 + ", That is today, good guess.");
+                }
+                else if (dayChoice == "thursday")
+                {
+                    Console.WriteLine("\n" + days4 + ", That is today, good guess.");
+                }
+                else if (dayChoice == "friday")
+                {
+                    Console.WriteLine("\n" + days5 + ", That is today, good guess.");
+                }
+                else if (dayChoice == "saturday")
+                {
+                    Console.WriteLine("\n" + days6 + ", That is today, good guess.");
+                }
+                else if (dayChoice == "sunday")
+                {
+                    Console.WriteLine("\n" + days7 + ", That is today, good guess.");
+                }
+                else
+                {
+
+                }
+                Console.ReadLine();
+            }
+            //catch for the try catch statement catching FormatExceptions
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+        }
+    }
+                
+}
+
+
+
+
+
+
+
