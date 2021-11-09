937768fcac63d7e9d9654b83d424ac95be9591db
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-11-09 14:25:54 -0800

    In the main() method, created a list of 10 employees, Each employee having a first and last name as well as an id number.  Two of the employees have the same first name(Joe).  Using a foreach loop, created a new list of all employees with the first name "Joe".  Perfomed the same action again with a lambda expression.  Using a lambda expression, made a list of all employees with an id number greater than 5.

---
+++ LambdaSubmissionAssignment/LambdaSubmissionAssignment/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaSubmissionAssignment
+{
+    public class Employee
+    {
+        //public variable for the blueprint of the list
+        public int _Id;
+        public string _firstName;
+        public string _lastName;
+
+        //method for taking in values for creating a list returning the values from each paramater passed to the method
+        public Employee(int Id, string firstName, string LastName)
+        {
+            _Id = Id;
+            _firstName = firstName;
+            _lastName = LastName;
+        }
+
+
+
+
+    }
+}
---
+++ LambdaSubmissionAssignment/LambdaSubmissionAssignment/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //created a list of of ints and strings 
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee(1, "Joe", "Smith"));
+            employees.Add(new Employee(2, "Fread", "Smith"));
+            employees.Add(new Employee(3, "Jeremiah", "Smith"));
+            employees.Add(new Employee(4, "Jasmine", "Smith"));
+            employees.Add(new Employee(5, "Desirea", "Smith"));
+            employees.Add(new Employee(6, "Chloe", "Smith"));
+            employees.Add(new Employee(7, "Grace", "Smith"));
+            employees.Add(new Employee(8, "Sara", "Smith"));
+            employees.Add(new Employee(9, "Susan", "Smith"));
+            employees.Add(new Employee(10, "Joe", "Scott"));
+
+            //for each loop iterating through the list employees
+            foreach (var I in employees)
+            {
+                //if statement comparing the value Joe through the list from the iteration of the foreach loop with _firstName.  If there is a mach, making a new list of just Joes.
+                if (I._firstName == "Joe")
+                {
+                    //new list for just Joe employees
+                    List<Employee> joe = new List<Employee>();
+                    joe.Add(I);
+                    //a foreach loop to pring to the console each value in the list
+                    foreach(var j in joe)
+                    {
+                        //Console.WriteLine($"Id: {j._Id} Name: {j._firstName} {j._lastName}");
+                    }
+
+                    //a lambda expresion creating a new list for just the value joe from the if statment comparing all the _firstName values in the list to Joe.
+                    new List<Employee>(joe).ForEach(i => Console.WriteLine($"Id: {i._Id} Name: {i._firstName} {i._lastName}"));
+                    
+
+                }
+               
+
+
+                
+            }
+            Console.WriteLine("");
+            //for each loop iterating through employees
+            foreach(var j in employees)
+            {
+                //if statement comparing all the id numbers to greater then 5 through the iteration of the foreach loop.
+                if (j._Id > 5)
+                {
+                    //creating a list of only id number eployees greater than 5.
+                    List<Employee> idnumber5 = new List<Employee>();
+                    idnumber5.Add(j);
+
+                    //a lambda expresion printing to console the value id number greater than 5 from the if statment comparing all the _id values in the list to > 5.
+                    new List<Employee>(idnumber5).ForEach(i => Console.WriteLine($"Id: {j._Id} Name: {j._firstName} {j._lastName}"));
+                    
+                }
+            }
+
+            
+
+            Console.ReadLine();
+
+
+
+        }
+
+
+    }
+}
