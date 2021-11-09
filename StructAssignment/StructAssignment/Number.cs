69eedc3f11fe494b8b867b02a8a2ac79e8706170
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-11-09 02:15:27 -0800

    StructAssignment. Created a struct called Number and gave it the property Amount, the data type is decimal.  In the Main() method, created an object of data type Number and assign an amount to it.

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    //Public struct Number
+    public struct Number
+    {
+        //a property of Struct Number: amount
+        public decimal amount { get; set; }
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //an object for data type Number with a number assigned to amount.
+            Number amount1 = new Number(){ amount = 466 };            
+
+            Console.WriteLine(amount1.amount);
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
