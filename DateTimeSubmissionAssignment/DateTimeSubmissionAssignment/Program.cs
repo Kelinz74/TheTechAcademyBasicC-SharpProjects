5ff94f0553e5b9d41f153e9f04772ce00d0a2aae
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-11-09 22:32:53 -0800

    created a DateTime.now method to print the current time and date that the program is ran.  Included an input for the user asking for a number.  Used the number to calculate how many hours from now what time and date it will be.

---
+++ DateTimeSubmissionAssignment/DateTimeSubmissionAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //console.Writeline method displaying the time right now. utilizing the datetime.now method
+            Console.WriteLine(DateTime.Now);
+
+            //Console.Wtiteline printing to the console a request for user input.
+            Console.WriteLine("\nPlease provide a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //a variable saving the value of todays date and time from when the program was ran.
+            DateTime today = DateTime.Now;
+
+            //a variable saving the date from todays date variable using the .addhours method to figure what time it will be from now to the future using the inputed number from the user
+            //as the number of hours from now.
+            DateTime answer = today.AddHours(number);
+
+            //a writeline method displaying the number from the user input and the variable answer from the days in the future calculation.
+            Console.WriteLine("\n{0} from today: {1}", number, answer);
+
+            Console.ReadLine();
+        }
+    }
+}
