945839f368f225eaaefd10c5062608cec7d1adf9
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-11-09 21:36:07 -0800

    Ask a user for an input.  Log the input to a text file using the System.IO file.writealltext method.  Display to the consol the inputed information from the saved text file using the System.lO.file.ReadAllText method printing to the console using the system.console.WriteLine method.

---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //a wtiteline printing the console a question asking for an input.
+            Console.WriteLine("Please give a number, any number.");
+            string number = Console.ReadLine();
+            //a write to file saving the input string from the user.
+            File.WriteAllText(@"C:\Users\Kelinz\Desktop\Basic_C#_Programs\IO test\logtest.txt", number);
+            //reading a textfile and printing its content to the console with the Console.Writeline method.
+            string text = System.IO.File.ReadAllText(@"C:\Users\Kelinz\Desktop\Basic_C#_Programs\IO test\logtest.txt");
+            System.Console.WriteLine("Contents of file: {0}", text);
+
+            Console.ReadLine();
+        }
+    }
+}
