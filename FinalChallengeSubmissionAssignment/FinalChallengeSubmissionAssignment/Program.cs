8434b9d20701d4c4051b69c0af0c1e18db28681a
Kelinz74 <76086747+Kelinz74@users.noreply.github.com>
2021-11-17 22:35:18 -0800

    Created a basic Entity Framework Code-First app that creates a basic Student database and adds one student.

---
+++ FinalChallengeSubmissionAssignment/FinalChallengeSubmissionAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace FinalChallengeSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            using (var db = new StudentContext())
+            {
+                
+                Console.WriteLine("What is your name?");
+                var studentName = Console.ReadLine();                
+
+                var nStudent = new Student { studentName = studentName };                
+
+                db.Students.Add(nStudent);                
+                db.SaveChanges();                
+            }
+           
+        }
+    }
+    public class Student
+    {        
+        public int StudentId { get; set; }
+        public string studentName { get; set; }
+        
+
+        public virtual Lazy<Student> Students { get; set; }
+    }
+
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
